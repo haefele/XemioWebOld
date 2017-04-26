74df0d11a89713bd0d2e204e3794793dc18eb02d
Daniel Häfele <haefele@xemio.net>
2017-04-26 21:49:44 +0200

    Make login screen okay'ish (#3)

--- web/src/app-logged-out.ts
+++ web/src/app-logged-out.ts
@@ -1,8 +1,8 @@
-import {BrowserService} from "services/browser/browser-service";
-import {AuthService} from "services/auth/auth-service";
-import {User} from "services/auth/user";
-import {autoinject} from "aurelia-framework";
-import {Command} from "helper/command";
+import { BrowserService } from "services/browser/browser-service";
+import { AuthService } from "services/auth/auth-service";
+import { User } from "services/auth/user";
+import { autoinject } from "aurelia-framework";
+import { Command } from "helper/command";
 import { RoutableComponentActivate, RouteConfig, NavigationInstruction, IObservable } from "aurelia-router";
 
 @autoinject()
@@ -12,7 +12,7 @@ export class AppLoggedOut implements RoutableComponentActivate {
     public facebookLoginCommand: Command;
     public googleLoginCommand: Command;
 
-    constructor(private readonly browserService: BrowserService, private readonly authService: AuthService) {        
+    constructor(private readonly browserService: BrowserService, private readonly authService: AuthService) {
         this.microsoftLoginCommand = new Command(() => this.authService.loginWithMicrosoft());
         this.facebookLoginCommand = new Command(() => this.authService.loginWithFacebook());
         this.googleLoginCommand = new Command(() => this.authService.loginWithGoogle());
