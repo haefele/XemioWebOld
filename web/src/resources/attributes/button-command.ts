c53bdbd1b503b852cbd65038462d1a0a7ca783a5
Daniel Häfele <haefele@xemio.net>
2017-04-27 22:26:43 +0200

    Change ButtonCommandCustomAttribute to use the BindingEngine

--- web/src/resources/attributes/button-command.ts
+++ web/src/resources/attributes/button-command.ts
@@ -1,101 +1,94 @@
-import {Command} from "helper/command";
-import {autoinject, ObserverLocator} from "aurelia-framework";
+import { Command } from "helper/command";
+import { autoinject, BindingEngine, Disposable } from "aurelia-framework";
 import * as $ from "jquery";
 
 @autoinject()
 export class ButtonCommandCustomAttribute {
-    private _onCommandIsExecutingChangedBind: (newValue: any, oldValue: any) => void;        
-    private _onCommandCanExecuteChangedBind: (newValue: any, oldValue: any) => void;
-    private _onButtonClickBind: () => Promise<void>;
+  private canExecuteSubscription: Disposable;
+  private isExecutingSubscription: Disposable;
+  private onButtonClickBind: () => Promise<void>;
 
-    private _command: Command;
-                
-    public constructor(private element: Element, private observerLocator: ObserverLocator) {
-        this._onCommandIsExecutingChangedBind = this.onCommandIsExecutingChanged.bind(this);
-        this._onCommandCanExecuteChangedBind = this.onCommandCanExecuteChanged.bind(this);
-        this._onButtonClickBind = this.onButtonClick.bind(this);
-    }
-    
-    public attached(): void {
-        this.element.addEventListener("click", this._onButtonClickBind);
-    }
-    
-    public detached(): void {
-        this.element.removeEventListener("click", this._onButtonClickBind);
-    }
-    
-    public valueChanged(newValue: Command, oldValue: Command): void {
-        this._command = newValue;
-        
-        if (oldValue) {
-            this.observerLocator
-                .getObserver(oldValue, "isExecuting")
-                .unsubscribe(this._onCommandIsExecutingChangedBind);         
-                
-            this.observerLocator
-                .getObserver(oldValue, "canExecute")
-                .unsubscribe(this._onCommandCanExecuteChangedBind);
-        }
-        
-        if (newValue) {
-            this.observerLocator
-                .getObserver(newValue, "isExecuting")
-                .subscribe(this._onCommandIsExecutingChangedBind);
-                
-            this.observerLocator
-                .getObserver(newValue, "canExecute")
-                .subscribe(this._onCommandCanExecuteChangedBind);
-                
-            this.onCommandIsExecutingChanged(this._command.isExecuting, undefined);
-            this.onCommandCanExecuteChanged(this._command.canExecute, undefined);                
-        }        
-    }
-    
-    private onCommandIsExecutingChanged(newValue: boolean, oldValue: boolean): void {
-        if (newValue === true) { 
-            this.addLoading();         
-        }
-        if (newValue === false) {
-            this.removeLoading();
-        }
-    }
-    private onCommandCanExecuteChanged(newValue: boolean, oldValue: boolean): void {
-        if (newValue === true) {
-            this.removeDisabled();
-        }
-        if (newValue === false) {
-            this.addDisabled();
-        }
-    }
-    private async onButtonClick(): Promise<void> {
-        if (this._command !== null) {
-            await this._command.execute();
-        }
+  private command: Command;
+
+  public constructor(private element: Element, private bindingEngine: BindingEngine) {
+    this.onButtonClickBind = this.onButtonClick.bind(this);
+  }
+
+  public attached(): void {
+    this.element.addEventListener("click", this.onButtonClickBind);
+  }
+
+  public detached(): void {
+    this.element.removeEventListener("click", this.onButtonClickBind);
+  }
+
+  public valueChanged(newValue: Command, oldValue: Command): void {
+    this.command = newValue;
+
+    if (oldValue) {
+      this.isExecutingSubscription.dispose();
+      this.canExecuteSubscription.dispose();
     }
 
-    private addLoading(): void {
-        let loadingMessage = this.element.getAttribute("button-command-loading-message");
+    if (newValue) {
+      this.isExecutingSubscription = this.bindingEngine
+        .propertyObserver(newValue, "isExecuting")
+        .subscribe((newValue, oldValue) => this.onCommandIsExecutingChanged(newValue, oldValue));
 
-        if (!loadingMessage)
-            loadingMessage = "Loading";
+      this.canExecuteSubscription = this.bindingEngine
+        .propertyObserver(newValue, "canExecute")
+        .subscribe((newValue, oldValue) => this.onCommandCanExecuteChanged(newValue, oldValue));
 
-        this.element.setAttribute("data-loading-text", "<style>.glyphicon-spin { animation: spin 700ms infinite linear; } @keyframes spin { 0% { transform: rotate(0deg); } 100% { transform: rotate(359deg); } }</style><span class='glyphicon glyphicon-refresh glyphicon-spin'></span> " + loadingMessage + "…");
-        $(this.element).button("loading");
+      this.onCommandIsExecutingChanged(this.command.isExecuting, undefined);
+      this.onCommandCanExecuteChanged(this.command.canExecute, undefined);
     }
-    private removeLoading(): void {
-        $(this.element).button("reset");
-        this.element.removeAttribute("data-loading-text");
+  }
 
-        //Make sure the button is correctly disabled when we finished executing the command
-        setTimeout(() => {
-            this.onCommandCanExecuteChanged(this._command.canExecute, undefined);
-        }, 0);
+  private onCommandIsExecutingChanged(newValue: boolean, oldValue: boolean): void {
+    if (newValue === true) {
+      this.addLoading();
     }
+    if (newValue === false) {
+      this.removeLoading();
+    }
+  }
+  private onCommandCanExecuteChanged(newValue: boolean, oldValue: boolean): void {
+    if (newValue === true) {
+      this.removeDisabled();
+    }
+    if (newValue === false) {
+      this.addDisabled();
+    }
+  }
+  private async onButtonClick(): Promise<void> {
+    if (this.command !== null) {
+      await this.command.execute();
+    }
+  }
 
-    private addDisabled(): void {
-        this.element.classList.add("disabled");
-    }
-    private removeDisabled(): void {
-        this.element.classList.remove("disabled");
-    }
+  private addLoading(): void {
+    let loadingMessage = this.element.getAttribute("button-command-loading-message");
+
+    if (!loadingMessage)
+      loadingMessage = "Loading";
+
+    this.element.setAttribute("data-loading-text", "<style>.glyphicon-spin { animation: spin 700ms infinite linear; } @keyframes spin { 0% { transform: rotate(0deg); } 100% { transform: rotate(359deg); } }</style><span class='glyphicon glyphicon-refresh glyphicon-spin'></span> " + loadingMessage + "…");
+    $(this.element).button("loading");
+  }
+  private removeLoading(): void {
+    $(this.element).button("reset");
+    this.element.removeAttribute("data-loading-text");
+
+    //Make sure the button is correctly disabled when we finished executing the command
+    setTimeout(() => {
+      this.onCommandCanExecuteChanged(this.command.canExecute, undefined);
+    }, 0);
+  }
+
+  private addDisabled(): void {
+    this.element.classList.add("disabled");
+  }
+  private removeDisabled(): void {
+    this.element.classList.remove("disabled");
+  }
 }
